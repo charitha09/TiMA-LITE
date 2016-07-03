a3e447469d29e2a0c15f4ebb5d1dff9ba6bcb08e
charitha09 <charitha009@gmail.com>
2016-07-03 18:39:23 +0530

    With Ionic Cordova

---
+++ www/js/app.js
@@ -0,0 +1,125 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('tima', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    //Tabs
+    .state('tabs', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "templates/tabs.html"
+    })
+
+    //Home
+    .state('tabs.home', {
+      url: "/home",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/home.html"
+        }
+      }
+    })
+    /*
+    .state('tabs.facts', {
+      url: "/facts",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/facts.html"
+        }
+      }
+    })
+    .state('tabs.facts2', {
+      url: "/facts2",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/facts2.html"
+        }
+      }
+    })*/
+
+    //My Events
+    .state('tabs.events', {
+      url: "/events",
+      views: {
+        'events-tab': {
+          templateUrl: "templates/events.html"
+        }
+      }
+    })
+    /*
+    .state('tabs.navstack', {
+      url: "/navstack",
+      views: {
+        'about-tab': {
+          templateUrl: "templates/nav-stack.html"
+        }
+      }
+    })*/
+
+    //Settings
+    .state('tabs.Settings', {
+      url: "/Settings",
+      views: {
+        'Settings-tab': {
+          templateUrl: "templates/settings.html"
+        }
+      }
+    });
+
+
+  $urlRouterProvider.otherwise("/tab/home");
+
+})
+
+
+.controller('indexController', function($scope, $ionicPopup) {
+  // Triggered on a button click, or some other target
+  $scope.CreateEventPopup = function() {
+    $scope.data = {}
+
+    // An elaborate, custom popup
+    var myPopup = $ionicPopup.show({
+      templateUrl: "templates/create_event.html",
+      title: 'Place new event',
+      scope: $scope,
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: '<b>Save</b>',
+          type: 'button-positive',
+          onTap: function(e) {
+            if (!$scope.data.wifi) {
+              //don't allow the user to close unless he enters wifi password
+              e.preventDefault();
+            } else {
+              return $scope.data.wifi;
+            }
+          }
+        }
+      ]
+    });
+  };
+});
